09297e407e6e0b839dc9eff2eb84de2b9f48b774
Sh0murod <147705628+Sh0murod@users.noreply.github.com>
2023-10-25 21:40:41 +0500

    Update main.py
    Shomurod Anvarov

--- src/py/main.py
+++ src/py/main.py
@@ -1,2 +1,57 @@
-# * Author:
-# * Date:
+# * Author:Shomurod Anvarov SSE1 20221400164
+# * Date: 25.10.2023
+1
+year = int(input("year: "))
+
+if year % 400 == 0:
+    leap = True
+elif year % 100 == 0:
+    leap = False
+elif year % 4 == 0:
+    leap = True
+else:
+    leap = False
+
+if leap:
+    print(year, " – leap year.")
+else:
+    print(year, " – non-leap year.")
+
+2
+
+import random
+
+correct = int(input("Write a correct number:"))
+random = random.randint(1, 100)
+while correct != random:
+ if correct < random:
+  print("low")
+  correct = int(input("Write a correct number:"))
+ elif correct > random:
+  print("high")
+  correct = int(input("Write a correct number:"))
+if correct == random:
+ print("Congratulations !!! You have found a number")
+
+3
+
+sentence = input("sentence: ")
+word = input("word to search for: ")
+sentence = sentence.lower()
+word = word.lower()
+if word in sentence:
+    index = sentence.find(word)
+    print(f"The word '{word}' was found at index {index} in the sentence.")
+else:
+    print("no match")
+
+4
+
+import random
+digits = [random.randint(0, 9) for _ in range(100)]
+number = int(''.join(map(str, digits)))
+print(number)
+number_A = int(input("number:"))
+result = number / number_A
+print(result)
+
